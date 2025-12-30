7a11644f59345f59fd64509310871cff1c0d5c43
Girijarajarapu <girijapavani09@gmail.com>
2025-12-30 18:43:41 +0530

    Add files via upload

---
+++ backend/alert_setting.py
@@ -0,0 +1,13 @@
+ENABLE_EMAIL_ALERTS = False
+ENABLE_SMS_ALERTS = False
+
+def get_settings():
+    return {
+        "email": ENABLE_EMAIL_ALERTS,
+        "sms": ENABLE_SMS_ALERTS
+    }
+
+def update_settings(email: bool, sms: bool):
+    global ENABLE_EMAIL_ALERTS, ENABLE_SMS_ALERTS
+    ENABLE_EMAIL_ALERTS = email
+    ENABLE_SMS_ALERTS = sms
